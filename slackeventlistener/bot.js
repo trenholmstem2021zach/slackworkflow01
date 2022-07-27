4c641c762bcc9d1652907a3272f2ddabf366b6a2
zach lewis <zlewis@clarityinnovates.com>
2022-07-27 16:53:46 -0500

    bot for support channel

---
+++ slackeventlistener/bot.js
@@ -0,0 +1,35 @@
+let foo = {
+  bot_id: 'B022J521AG0',
+  type: 'message',
+  text: 'Hey there <@U02QZ7YGDJA>, you have a new Platform Support ticket!',
+  user: 'U021MHE6N22',
+  ts: '1658955368.442689',
+  app_id: 'A021UFCT9LK',
+  team: 'T03FECE8V',
+  bot_profile: {
+    id: 'B022J521AG0',
+    deleted: false,
+    name: 'Platform support',
+    updated: 1620919272,
+    app_id: 'A021UFCT9LK',
+    icons: {
+      image_36: 'https://avatars.slack-edge.com/2021-05-13/2062334410354_244f716aa1868907af66_36.png',
+      image_48: 'https://avatars.slack-edge.com/2021-05-13/2062334410354_244f716aa1868907af66_48.png',
+      image_72: 'https://avatars.slack-edge.com/2021-05-13/2062334410354_244f716aa1868907af66_72.png'
+    },
+    team_id: 'T03FECE8V'
+  },
+  blocks: [
+    { type: 'section', block_id: '8di', text: [Object] },
+    { type: 'section', block_id: 'WzG', text: [Object] },
+    {
+      type: 'section',
+      block_id: 'YBz4v',
+      text: [Object],
+      accessory: [Object]
+    }
+  ],
+  channel: 'CBU0KDSB1',
+  event_ts: '1658955368.442689',
+  channel_type: 'channel'
+}
--- slackeventlistener/messageHandler.js
+++ slackeventlistener/messageHandler.js
@@ -3,25 +3,32 @@ module.exports =
         function (app, botToken, slackChannelId, logger) {
             {
                 app.message(async ({message, say}) => {
-                    if (message.thread_ts === undefined) {
-                        if (message.subtype !== undefined && message.subtype === 'bot_message') {
-                            console.log(message)
-                            const m1 = message.text.split("\*")
-                            console.log(m1)
-                            await app.client.chat.postMessage({
-                                token: botToken,
-                                channel: slackChannelId,
-                                thread_ts: message.ts,
-                                text: "Support Requests Working On It"
-                            })
-                            // Add call another work on it function.
-                        } else {
-                            logger.info("Message Received Ignore Future TBD ... :" + message.text)
-                        }
+                    if (message.channel === slackChannelId) {
+                        if (message.thread_ts === undefined && message.bot_id === 'B022J521AG0') {
+                            if (message.subtype !== undefined && message.subtype === 'bot_message') {
+                                console.log(message)
+                                const m1 = message.text.split("\*")
+                                console.log(m1)
+                               /* await app.client.chat.postMessage({
+                                    token: botToken,
+                                    channel: slackChannelId,
+                                    thread_ts: message.ts,
+                                    text: "Support Requests Working On It"
+                                }) */
+                                // Add call another work on it function.
+                            } else {
+                                console.log(message)
+                                // logger.info("Message Received Ignore Future TBD ... :" + message.text)
+                            }
 
+                        } else {
+                            //Thread
+                            console.log(message)
+                            //logger.info("Thread message:  Future TBD ..." + message.type)
+                        }
                     } else {
-                        //Thread
-                        logger.info("Thread message:  Future TBD ...")
+                        console.log(message.channel)
+                        console.log(message.text)
                     }
                 });
 
